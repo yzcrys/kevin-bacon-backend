d8d880fffb451c6a3e47582335288a98fa22b71b
yzcrys <deaddyui@gmail.com>
2022-10-26 12:11:33 -0400

    Create addRelationship API endpoint, create addRelationshipPass/addRelationshipFail tests

--- src/main/java/ca/utoronto/utm/mcs/Neo4jDAO.java
+++ src/main/java/ca/utoronto/utm/mcs/Neo4jDAO.java
@@ -24,7 +24,7 @@ public class Neo4jDAO {
     public boolean actorExists(String actorId) {
         try(Session session = driver.session()){
             Transaction tx = session.beginTransaction();
-            String query = "MATCH (n: Actor) WHERE n.actorId = '%s' RETURN n".formatted(actorId);
+            String query = "MATCH (n: actor) WHERE n.actorId = '%s' RETURN n".formatted(actorId);
             Result res = tx.run(query);
             Boolean exists = res.hasNext();
             tx.commit();
@@ -37,12 +37,12 @@ public class Neo4jDAO {
             Transaction tx = session.beginTransaction();
 
             if(actorExists(actorId)) {
-                System.out.println("Neo4jDAO: addActor(): Actor " + name + " already exists");
+                System.out.println("Neo4jDAO: addActor(): actor " + name + " already exists");
                 return 400;
             }
             else {
-                System.out.println("Neo4jDAO: addActor(): Adding Actor " + name);
-                String query = "CREATE (n: Actor {name:'%s', actorId:'%s'})".formatted(name, actorId);
+                System.out.println("Neo4jDAO: addActor(): Adding actor " + name);
+                String query = "CREATE (n: actor {name:'%s', actorId:'%s'})".formatted(name, actorId);
                 tx.run(query);
                 tx.commit();
                 return 200;
@@ -57,7 +57,7 @@ public class Neo4jDAO {
     public boolean movieExists(String movieId) {
         try(Session session = driver.session()){
             Transaction tx = session.beginTransaction();
-            String query = "MATCH (n: Movie) WHERE n.movieId = '%s' RETURN n".formatted(movieId);
+            String query = "MATCH (n: movie) WHERE n.movieId = '%s' RETURN n".formatted(movieId);
             Result res = tx.run(query);
             Boolean exists = res.hasNext();
             tx.commit();
@@ -75,7 +75,46 @@ public class Neo4jDAO {
             }
             else {
                 System.out.println("Neo4jDAO: addMovie(): Adding movie " + name);
-                String query = "CREATE (n: Movie {name:'%s', movieId:'%s'})".formatted(name, movieId);
+                String query = "CREATE (n: movie {name:'%s', movieId:'%s'})".formatted(name, movieId);
+                tx.run(query);
+                tx.commit();
+                return 200;
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            return 500;
+        }
+    }
+
+    public boolean relationshipExists(String actorId, String movieId) {
+        try(Session session = driver.session()){
+            Transaction tx = session.beginTransaction();
+            String query = "MATCH (a:actor {actorId: '%s'})-[r:ACTED_IN]->(b:movie {movieId: '%s'}) RETURN a".formatted(actorId, movieId);
+            Result res = tx.run(query);
+            Boolean exists = res.hasNext();
+            tx.commit();
+            return exists;
+        }
+    }
+
+    public int addRelationship(String actorId, String movieId) {
+        try(Session session = driver.session()){
+            Transaction tx = session.beginTransaction();
+
+            if (!actorExists(actorId) || !movieExists(movieId)) {
+                System.out.println("Neo4jDAO: addRelationship():  " + actorId + " or " + movieId + "does not exist");
+                return 404;
+            }
+            else if (relationshipExists(actorId, movieId)) {
+                System.out.println("Neo4jDAO: addRelationship(): relationship " + actorId + " ACTED_IN " + movieId + "already exists");
+                return 400;
+            }
+            else {
+                System.out.println("Neo4jDAO: addMovie(): Adding relationship " + actorId + " ACTED_IN " + movieId);
+                String query = ("MATCH (a:actor), (b:movie) WHERE a.actorId = '%s' AND b.movieId = '%s'" +
+                        " CREATE (a)-[r:ACTED_IN]->(b)" +
+                        " SET a.movies = [(a)-->(c:movie) | c.movieId]").formatted(actorId, movieId);
                 tx.run(query);
                 tx.commit();
                 return 200;
